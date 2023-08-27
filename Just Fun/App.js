e5c258f1cecbbe7137a88fdc34ad1db5a4eec386
hA2zy <haz2y@dsm.hs.kr>
2023-08-27 21:11:48 +0900

    react

---
+++ Just Fun/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Side from "./side_bar";
+
+function App() {
+  const { kakao } = window;
+  const [mapCreated, setMapCreated] = useState(false);
+
+  useEffect(() => {
+    if (kakao && !mapCreated) {
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      const map = new kakao.maps.Map(container, options);
+      setMapCreated(true);
+    }
+  }, [kakao, mapCreated]); // kakao와 mapCreated가 변경될 때마다 실행
+
+  return (
+    <>
+    <div
+      className="map"
+      style={{ width: "100wv", height: "100vh" }}
+      id="map"
+    ></div>
+    <Side/>
+    </>
+  );
+}
+
+export default App;
---
+++ Just Fun/side_bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./css/App.css";
+
+function Side() {
+  return (
+    <>
+      <div className="Side_bar"></div>
+    </>
+  );
+}
+
+export default Side;
