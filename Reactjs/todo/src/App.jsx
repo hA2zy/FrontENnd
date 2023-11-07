e1940eb4f36cbeb4febf93dbc7fc1fb4d5832187
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-11-07 22:42:48 +0900

    TODO로 각종 연습

--- Reactjs/todo/src/App.jsx
+++ Reactjs/todo/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <>
-    </>
+    <Container>
+      <Header></Header>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  
+`
+
+const Header = styled.div`
+  
+`
 
 export default App;
