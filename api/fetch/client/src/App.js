209c83127717eaff8f4c06bdf68c6956ab921d3d
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-11-14 17:33:01 +0900

    API fetch를 공부하고 있습니다. (server 개발 완료)

--- api/fetch/client/src/App.js
+++ api/fetch/client/src/App.js
@@ -1,44 +1,11 @@
-import { useEffect, useState } from "react";
+import React from 'react';
 
 function App() {
-    const [todoList, setTodoList] = useState(null);
-
-    useEffect(() => {
-        fetch("http://localhost:4000/api/todo")
-            .then((response) => response.json())
-            .then((data) => setTodoList(data));
-    }, []);
-
-    const onSubmitHandler = (e) => {
-        e.preventDefalult();
-        const text = e.target.text.value;
-        const done = e.target.done.value;
-        fetch("http://localhost:4000/api/todo", {
-            method: "POST",
-            body: JSON.stringify({
-                text,
-                done,
-            }),
-        });
-    };
-
-    return (
-        <div className="App">
-            <h1>TODO LIST</h1>
-            <form onSubmit={onSubmitHandler}>
-                <input name="text" />
-                <input name="done" type="checkbox" />
-                <input type="submit" value={"추가"} />
-            </form>
-            {todoList?.map((todo) => (
-                <div key={todo.id}>
-                    <div>{todo.id}</div>
-                    <div>{todo.text}</div>
-                    <div>{todo.done ? "Y" : "N"}</div>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div className="App">
+      
+    </div>
+  );
 }
 
 export default App;
