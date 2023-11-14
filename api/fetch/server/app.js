7f2cffe65d84d7ccc7f18e19e4231ac6bb695bb6
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-11-14 19:36:06 +0900

    fetch API연결 코드 완료 입니다.

--- api/fetch/server/app.js
+++ api/fetch/server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors())
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
