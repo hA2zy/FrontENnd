9daed5db246f85eae99e0927dc8d7c60cd27d885
hA2zy <128462388+hA2zy@users.noreply.github.com>
2024-01-03 20:18:07 +0900

    잡

--- TypeScript/TypeScript연습/src/index.ts
+++ TypeScript/TypeScript연습/src/index.ts
@@ -41,8 +41,102 @@
 // };
 
 // enum
-enum Os {
-  Window,
-  ios,
-  Android,
+// enum Os {
+//   Window,
+//   ios,
+//   Android,
+// }
+
+// type Score = "A" | "B" | "C" | "D" | "E" | "F";
+// interface User {
+//   name: string;
+//   age: number;
+//   gender?: string;
+//   readonly birthYear: number;
+//   [grade: number]: Score;
+// }
+
+// let user: User = {
+//   name: "xxx",
+//   age: 30,
+//   birthYear: 2000,
+//   1: "A",
+// };
+
+// user.gender = "M";
+
+// console.log(user.name);
+
+// interface Add {
+//   (num1: number, num2: number): number;
+// }
+
+// const add: Add = (x, y) => {
+//   return x * y;
+// };
+
+// add(10, 20);
+
+// interface IsAdult {
+//   (age: number): boolean;
+// }
+
+// const Age: IsAdult = (age) => {
+//   return age > 19;
+// };
+
+// Age(10);
+
+// console.log(Age(20));
+
+// interface Benz extends Car {
+//   door: number;
+//   stop(): void;
+// }
+
+// const benz: Benz = {
+//   color: 'black',
+//   wheels: 4,
+//   start: () => {
+//     console.log("Go!!");
+//   },
+//   door: 5,
+//   stop: () => {
+//     console.log("Stop..");
+//   },
+// };
+
+// console.log(benz);
+
+// class BMW implements Car {
+//   color;
+//   wheels = 4;
+
+//   constructor(e: string) {
+//     this.color = e;
+//   }
+
+//   start() {
+//     console.log("GO!!");
+//   }
+// }
+
+// const b = new BMW("RED");
+
+// console.log(b);
+
+// b.start();
+
+interface Car {
+  color: string;
+  wheels: number;
+  start(): void;
+}
+
+interface Toy {
+  name: string;
+}
+
+interface ToyCar extends Car, Toy {
+  price: number;
 }
